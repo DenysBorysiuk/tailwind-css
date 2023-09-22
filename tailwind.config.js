e98b59162ef5f7236b7a95825e9811eb6cffd6ef
Denys Borysiuk <denisborisiuk@gmail.com>
2023-09-22 19:51:13 +0300

    final commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: ['./index.html'],
   // darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        main: ['Montserrat'],
+      },
+    },
   },
   plugins: [],
 };
